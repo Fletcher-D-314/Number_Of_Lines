f4256c0558ab6ca370effc5d3fb4460c61bf4d74
Fletcher Doolan <fletcher.doolan@my.jcu.edu.au>
2020-02-23 21:45:29 +1000

    Add ability to use any grid size

--- src/NumberOfLines.java
+++ src/NumberOfLines.java
@@ -6,23 +6,24 @@ public class NumberOfLines {
 
     public static void main(String[] args) {
 
-        int gridSize = 3;
+        int gridSize = 5;
         int[][] grid = new int[gridSize][gridSize];
         int x = 0;
         int y = 0;
-boolean isRunning = true;
-        while (isRunning){
+        boolean isRunning = true;
+
+        while (isRunning) {
             grid[y][x] = 1;
             for (int[] row : grid) {
                 System.out.println(Arrays.toString(row));
             }
             System.out.println();
-            if (x == 2) {
+            if (x == gridSize - 1) {
                 x = -1;
                 y++;
             }
             x++;
-            if (y ==3){
+            if (y == gridSize) {
                 isRunning = false;
             }
         }
