dae37972407226961e4ce75deb6d3bc6114b5967
Fletcher Doolan <fletcher.doolan@my.jcu.edu.au>
2020-02-23 21:43:58 +1000

    Add ability to fill a 3x3 grid with 1's from row to column

--- src/NumberOfLines.java
+++ src/NumberOfLines.java
@@ -1,4 +1,6 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class NumberOfLines {
 
@@ -6,9 +8,23 @@ public class NumberOfLines {
 
         int gridSize = 3;
         int[][] grid = new int[gridSize][gridSize];
-
-        for (int[] line : grid) {
-            System.out.println(Arrays.toString(line));
+        int x = 0;
+        int y = 0;
+boolean isRunning = true;
+        while (isRunning){
+            grid[y][x] = 1;
+            for (int[] row : grid) {
+                System.out.println(Arrays.toString(row));
+            }
+            System.out.println();
+            if (x == 2) {
+                x = -1;
+                y++;
+            }
+            x++;
+            if (y ==3){
+                isRunning = false;
+            }
         }
     }
 }
