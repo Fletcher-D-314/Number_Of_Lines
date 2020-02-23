a0803ff4a5cd672fb2934da4fb7e5912490831c9
Fletcher Doolan <fletcher.doolan@my.jcu.edu.au>
2020-02-24 00:17:53 +1000

    Add an array of stored choices. The final array for a 3x3 grid will be the same as 9C3 (84). The array does not take into account the shape of the line but only has 3 different choices. (e.g the combinations like [0,3,4] are allowed even though that spans 4 points)

--- src/NumberOfLines.java
+++ src/NumberOfLines.java
@@ -1,4 +1,6 @@
+import java.awt.*;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
@@ -7,6 +9,7 @@ public class NumberOfLines {
     public static void main(String[] args) {
 
         int gridSize = 3;
+        ArrayList<int[]> positions = new ArrayList<>(0);
         int num1 = 0;
         int num2 = 0;
         int num3 = 0;
@@ -15,23 +18,30 @@ public class NumberOfLines {
 
         while (run) {
 
-            if (num1 == (gridSize*gridSize)) {
+            if (num1 == (gridSize * gridSize)) {
                 i = 0;
                 num1 = 0;
                 num2++;
             }
-            if (num2 == (gridSize*gridSize)) {
+            if (num2 == (gridSize * gridSize)) {
                 num2 = 0;
                 num3++;
             }
-            if (num3 == (gridSize*gridSize)) {
+            if (num3 == (gridSize * gridSize)) {
                 run = false;
             } else {
 
-                if (num1!=num2){
-                    if (num1 != num3){
-                        if (num2 != num3){
-                            System.out.println("num1 = " + num1 + "\tnum2 = " + num2 + "\tnum3 = " + num3);
+                if (num1 != num2) {
+                    if (num1 != num3) {
+                        if (num2 != num3) {
+                            int[] newPositions = {num1, num2, num3};
+                            Arrays.sort(newPositions);
+                            if (positions.size() == 0) {
+                                positions.add(newPositions);
+                            } else if (!isInArray(newPositions, positions)) {
+                                positions.add(newPositions);
+                            }
+                            System.out.println("num1 = " + num1 + "\tnum2 = " + num2 + "\tnum3 = " + num3 + "\t");
                         }
                     }
                 }
@@ -40,5 +50,20 @@ public class NumberOfLines {
                 num1 = i;
             }
         }
+        for (int[] position : positions) {
+            System.out.print(Arrays.toString(position) + ", ");
+        }
     }
+
+    private static boolean isInArray(int[] newPosition, ArrayList<int[]> positions) {
+        boolean isPresent = false;
+        for (int[] position : positions) {
+            if (Arrays.equals(position, newPosition)) {
+                isPresent = true;
+                break;
+            }
+        }
+        return isPresent;
+    }
+
 }
